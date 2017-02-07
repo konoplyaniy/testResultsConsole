1241c196df7d1635f32db9591536b5671f34e7a4
Fru <frolovdimon22@gmail.com>
2017-02-07 16:01:36 +0200

    add exporter xls and pdf

--- src/main/java/web/exporter/EventTableExporter.java
+++ src/main/java/web/exporter/EventTableExporter.java
@@ -2,6 +2,12 @@ package web.exporter;
 
 import hibernate.entities.EventEntity;
 import hibernate.service.EventService;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -22,6 +28,20 @@ public class EventTableExporter {
     public void init() {
         eventService = new EventService();
         events = eventService.findAll();
+    }
+
+    public void postProcessXLS(Object document) {
+        HSSFWorkbook wb = (HSSFWorkbook) document;
+        HSSFSheet sheet = wb.getSheetAt(0);
+        CellStyle style = wb.createCellStyle();
+        style.setFillBackgroundColor(IndexedColors.AQUA.getIndex());
+
+        for (Row row : sheet) {
+            for (Cell cell : row) {
+                cell.setCellValue(cell.getStringCellValue().toUpperCase());
+                cell.setCellStyle(style);
+            }
+        }
     }
 
     public List<EventEntity> getEvents() {
