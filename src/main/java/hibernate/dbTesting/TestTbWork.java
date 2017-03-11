a5cd2aebaacca90e708acc891c5de3a6dd57f7e4
geser <s.konoplyaniy@gmail.com>
2017-03-11 22:50:37 +0200

    commit just for test

--- src/main/java/hibernate/dbTesting/TestTbWork.java
+++ src/main/java/hibernate/dbTesting/TestTbWork.java
@@ -1,7 +1,6 @@
 package hibernate.dbTesting;
 
 
-import hibernate.entities.*;
 import hibernate.service.EventService;
 
 import java.net.InetAddress;
@@ -12,7 +11,6 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 /**
  * Created by Sergiy.K on 25-Jan-17.
@@ -36,6 +34,47 @@ public class TestTbWork {
 
 
     public static void main(String[] args) {
+        Date begining;
+        Calendar calendar_start = Calendar.getInstance();
+        calendar_start.set(Calendar.DAY_OF_MONTH,calendar_start.getActualMinimum(Calendar.DAY_OF_MONTH));
+        begining = calendar_start.getTime();
+        System.out.println(begining);
+
+        /*EventService service = new EventService();
+        System.out.println(service.findByCurrentMonthEvents());
+
+*/
+  /*      String query = "INSERT INTO `event` VALUES " +
+        "(1,'2017-01-26 16:14:29',2,5,3,3,NULL,NULL,NULL,1,0),";*/
+
+        /*"(5,'2017-01-26 17:51:08',5,7,5,5,NULL,NULL,NULL,1,1)," +
+        "(7,'2017-01-26 17:58:32',6,7,5,5,NULL,NULL,NULL,1,0)," +
+        "(11,'2017-01-26 18:15:19',7,7,5,5,NULL,NULL,NULL,1,1)," +
+        "(12,'2017-01-26 18:15:37',7,7,5,5,NULL,NULL,NULL,1,0)," +
+        "(13,'2017-01-26 18:16:01',7,7,5,5,NULL,NULL,NULL,1,1)," +
+        "(14,'2017-01-26 18:17:56',7,7,5,5,NULL,NULL,NULL,1,0)," +
+        "(16,'2017-01-26 18:19:49',7,7,5,5,NULL,NULL,NULL,1,0)," +
+        "(17,'2017-01-26 18:21:27',7,7,5,5,NULL,NULL,NULL,1,1)," +
+        "(18,'2017-01-26 18:22:52',7,7,5,5,NULL,NULL,NULL,1,1)," +
+        "(20,'2017-01-26 18:30:23',8,8,6,6,NULL,NULL,NULL,1,0)," +
+        "(21,'2017-01-26 18:33:12',9,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(22,'2017-01-26 18:33:51',9,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(23,'2017-01-27 16:31:35',9,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(24,'2017-01-27 16:35:32',10,8,6,6,NULL,NULL,NULL,1,0)," +
+        "(25,'2017-01-27 16:36:48',10,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(26,'2017-01-27 16:36:48',10,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(27,'2017-01-27 16:44:56',10,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(28,'2017-01-27 16:44:56',10,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(29,'2017-01-27 16:53:53',10,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(30,'2017-01-27 16:53:53',10,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(32,'2017-01-31 17:36:02',10,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(34,'2017-01-31 17:36:54',10,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(35,'2017-01-31 17:36:54',10,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(36,'2017-01-31 18:11:14',10,8,6,6,NULL,NULL,NULL,1,1)," +
+        "(37,'2017-01-31 18:11:14',10,8,6,6,NULL,NULL,NULL,1,1);";*/
+
+
+
 //        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 //        Date startDate = new Date();
 //        try {
