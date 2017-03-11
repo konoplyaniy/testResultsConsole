951933018f6bdce0de5319d19c3fd76853e5d71b
Sergiy.K <sergiy.k@dreamscapenetworks.com>
2017-03-11 16:04:55 +0200

    start fix app issue  with hibernate cache

--- src/main/java/hibernate/dao/EventDao.java
+++ src/main/java/hibernate/dao/EventDao.java
@@ -3,10 +3,7 @@ package hibernate.dao;
 import hibernate.entities.EventEntity;
 import hibernate.service.BrowserService;
 import org.apache.poi.ss.formula.functions.Even;
-import org.hibernate.CacheMode;
-import org.hibernate.Criteria;
-import org.hibernate.Query;
-import org.hibernate.SQLQuery;
+import org.hibernate.*;
 import org.primefaces.model.chart.LineChartSeries;
 
 import java.util.ArrayList;
@@ -33,80 +30,106 @@ public class EventDao extends BaseDao<Integer, EventEntity> {
         return event;
     }
 
-    public List<EventEntity> findByTestName(String testName) {
+    public ArrayList<EventEntity> findByTestName(String testName) {
         Query query = getCurrentSession().createQuery("from EventEntity where testByTestId.name =:testName ");
         query.setParameter("testName", testName);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findByGroupName(String groupName) {
+    public ArrayList<EventEntity> findByGroupName(String groupName) {
         Query query = getCurrentSession().createQuery("from EventEntity where testByTestId.groupByGroupId.name =:groupName");
         query.setParameter("groupName", groupName);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findByClassName(String className) {
+    public ArrayList<EventEntity> findByClassName(String className) {
         Query query = getCurrentSession().createQuery("from EventEntity where testByTestId.clazzByClassId.name =:className ");
         query.setParameter("className", className);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findBetweenDates(Date startSate, Date endDate) {
+    public ArrayList<EventEntity> findBetweenDates(Date startSate, Date endDate) {
         Query query = getCurrentSession().createQuery("from EventEntity where data between :startDate and :endDate");
         query.setParameter("startDate", startSate);
         query.setParameter("endDate", endDate);
-        List<EventEntity> events = (List<EventEntity>) query.list();
-        return events;
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
 
-    public List<EventEntity> findByTestNameBetweenDates(String testName, Date startDate, Date endDate) {
+    public ArrayList<EventEntity> findByTestNameBetweenDates(String testName, Date startDate, Date endDate) {
         Query query = getCurrentSession().createQuery("from EventEntity " +
                 "where testByTestId.name =:testName and data between :startDate and :endDate");
         query.setCacheMode(CacheMode.IGNORE);
         query.setParameter("testName", testName);
         query.setParameter("startDate", startDate);
         query.setParameter("endDate", endDate);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findByMonthEvents(Date date) {
+    public ArrayList<EventEntity> findByMonthEvents(Date date) {
         Query query = getCurrentSession().createQuery("from EventEntity " +
                 "where MONTH(data) = MONTH(:date) ");
         query.setParameter("date", date);
         query.setCacheMode(CacheMode.IGNORE);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findByDayEvents(Date date) {
+    public ArrayList<EventEntity> findByDayEvents(Date date) {
         Query query = getCurrentSession().createQuery("from EventEntity " +
                 "where DAY(data) = DAY(:date) ");
         query.setParameter("date", date);
         query.setCacheMode(CacheMode.IGNORE);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findBySyswebBetweenDates(String sysweb, Date startDate, Date endDate) {
+    public ArrayList<EventEntity> findBySyswebBetweenDates(String sysweb, Date startDate, Date endDate) {
         Query query = getCurrentSession().createQuery("from EventEntity " +
                 "where syswebBySyswebId.name =:sysweb and data between :startDate and :endDate");
         query.setCacheMode(CacheMode.IGNORE);
         query.setParameter("sysweb", sysweb);
         query.setParameter("startDate", startDate);
         query.setParameter("endDate", endDate);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findByClassNameBetweenDates(String clazzName, Date startDate, Date endDate) {
+    public ArrayList<EventEntity> findByClassNameBetweenDates(String clazzName, Date startDate, Date endDate) {
         Query query = getCurrentSession().createQuery("from EventEntity " +
                 "where testByTestId.clazzByClassId.name =:clazzName and data between :startDate and :endDate");
         query.setCacheMode(CacheMode.IGNORE);
         query.setParameter("clazzName", clazzName);
         query.setParameter("startDate", startDate);
         query.setParameter("endDate", endDate);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findByClassNameTestNameBetweenDates(String clazzName, String testName, Date startDate, Date endDate) {
+    public ArrayList<EventEntity> findByClassNameTestNameBetweenDates(String clazzName, String testName, Date startDate, Date endDate) {
         Query query = getCurrentSession().createQuery("from EventEntity " +
                 "where testByTestId.clazzByClassId.name =:clazzName " +
                 "and testByTestId.name =:testName " +
@@ -116,7 +139,10 @@ public class EventDao extends BaseDao<Integer, EventEntity> {
         query.setParameter("testName", testName);
         query.setParameter("startDate", startDate);
         query.setParameter("endDate", endDate);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
     public ArrayList<EventEntity> findBySelectedDay(String clazzName, String testName, String sysweb, String locale, Date date){
@@ -151,7 +177,9 @@ public class EventDao extends BaseDao<Integer, EventEntity> {
             query.setParameter("locale", locale);
         }
         query.setParameter("date", date);
+        Transaction transaction = getCurrentSession().beginTransaction();
         ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
         return results;
     }
 
@@ -187,16 +215,13 @@ public class EventDao extends BaseDao<Integer, EventEntity> {
         }
         query.setParameter("startDate", startDate);
         query.setParameter("endDate", endDate);
+        Transaction transaction = getCurrentSession().beginTransaction();
         ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
         return results;
     }
 
-    /*String sql = "SELECT first_name, salary FROM EMPLOYEE";
-
-
-    List results = query.list();*/
-
-    public List<EventEntity> findByClassNameTestNameSyswebBetweenDates(String clazzName, String testName, String sysweb, Date startDate, Date endDate) {
+    public ArrayList<EventEntity> findByClassNameTestNameSyswebBetweenDates(String clazzName, String testName, String sysweb, Date startDate, Date endDate) {
         Query query = getCurrentSession().createQuery("from EventEntity " +
                 "where testByTestId.clazzByClassId.name =:clazzName " +
                 "and testByTestId.name =:testName " +
@@ -208,10 +233,13 @@ public class EventDao extends BaseDao<Integer, EventEntity> {
         query.setParameter("sysweb", sysweb);
         query.setParameter("startDate", startDate);
         query.setParameter("endDate", endDate);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findByClassNameTestNameSyswebLocaleBetweenDates(String clazzName, String testName, String sysweb,
+    public ArrayList<EventEntity> findByClassNameTestNameSyswebLocaleBetweenDates(String clazzName, String testName, String sysweb,
                                                                              String locale, Date startDate, Date endDate) {
         Query query = getCurrentSession().createQuery("from EventEntity " +
                 "where testByTestId.clazzByClassId.name =:clazzName " +
@@ -226,11 +254,14 @@ public class EventDao extends BaseDao<Integer, EventEntity> {
         query.setParameter("locale", locale);
         query.setParameter("startDate", startDate);
         query.setParameter("endDate", endDate);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
 
-    public List<EventEntity> findBySyswebTestNameBetweenDates(String sysweb, String testName, Date startDate, Date endDate) {
+    public ArrayList<EventEntity> findBySyswebTestNameBetweenDates(String sysweb, String testName, Date startDate, Date endDate) {
         Query query = getCurrentSession().createQuery("from EventEntity " +
                 "where syswebBySyswebId.name =:sysweb " +
                 "and testByTestId.name =:testName " +
@@ -240,10 +271,13 @@ public class EventDao extends BaseDao<Integer, EventEntity> {
         query.setParameter("testName", testName);
         query.setParameter("startDate", startDate);
         query.setParameter("endDate", endDate);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findBySyswebTestNameLocaleBetweenDates(String sysweb, String testName, String locale, Date startDate, Date endDate) {
+    public ArrayList<EventEntity> findBySyswebTestNameLocaleBetweenDates(String sysweb, String testName, String locale, Date startDate, Date endDate) {
         Query query = getCurrentSession().createQuery("from EventEntity" +
                 " where syswebBySyswebId.name =:sysweb " +
                 "and testByTestId.name =:testName  " +
@@ -255,51 +289,75 @@ public class EventDao extends BaseDao<Integer, EventEntity> {
         query.setParameter("sysweb", sysweb);
         query.setParameter("startDate", startDate);
         query.setParameter("endDate", endDate);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findByLocale(String localeName) {
+    public ArrayList<EventEntity> findByLocale(String localeName) {
         Query query = getCurrentSession().createQuery("from EventEntity where localeByLocaleId.locale =:localeName ");
         query.setParameter("localeName", localeName);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findBySysweb(String sysweb) {
+    public ArrayList<EventEntity> findBySysweb(String sysweb) {
         Query query = getCurrentSession().createQuery("from EventEntity where syswebBySyswebId.name =:sysweb");
         query.setParameter("sysweb", sysweb);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findByPcName(String pcName) {
+    public ArrayList<EventEntity> findByPcName(String pcName) {
         Query query = getCurrentSession().createQuery("from EventEntity where pcByPcId.name =:pcName");
         query.setParameter("pcName", pcName);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findByPcOS(String pcOs) {
+    public ArrayList<EventEntity> findByPcOS(String pcOs) {
         Query query = getCurrentSession().createQuery("from EventEntity where pcByPcId.os =:pcOs");
         query.setParameter("pcOs", pcOs);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findByBrowser(String browser) {
+    public ArrayList<EventEntity> findByBrowser(String browser) {
         Query query = getCurrentSession().createQuery("from EventEntity where browserByBrowserId.browser =:browser");
         query.setParameter("browser", browser);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findOnlyChecked() {
+    public ArrayList<EventEntity> findOnlyChecked() {
         int checkStatus = 1;
         Query query = getCurrentSession().createQuery("from EventEntity where checked =:checkStatus");
         query.setParameter("checkStatus", checkStatus);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
-    public List<EventEntity> findOnlyUnChecked() {
+    public ArrayList<EventEntity> findOnlyUnChecked() {
         int checkStatus = 0;
         Query query = getCurrentSession().createQuery("from EventEntity where checked =:checkStatus");
         query.setParameter("checkStatus", checkStatus);
-        return (List<EventEntity>) query.list();
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> results = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
+        return results;
     }
 
     @Override
@@ -308,9 +366,12 @@ public class EventDao extends BaseDao<Integer, EventEntity> {
     }
 
     @SuppressWarnings("unchecked")
-    public List<EventEntity> findAll() {
-        getCurrentSession().setCacheMode(CacheMode.IGNORE);
-        List<EventEntity> events = (List<EventEntity>) getCurrentSession().createQuery("from EventEntity ").list();
+    public ArrayList<EventEntity> findAll() {
+        Query query = getCurrentSession().createQuery("from EventEntity ");
+        query.setCacheMode(CacheMode.IGNORE);
+        Transaction transaction = getCurrentSession().beginTransaction();
+        ArrayList<EventEntity> events = (ArrayList<EventEntity>) query.list();
+        transaction.commit();
         return events;
     }
 
