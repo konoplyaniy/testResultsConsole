611793dad168c21b1934c9893215f458734b8af6
Sergiy.K <sergiy.k@dreamscapenetworks.com>
2017-03-11 16:12:57 +0200

    change style, small deletes

--- src/main/java/hibernate/service/EventService.java
+++ src/main/java/hibernate/service/EventService.java
@@ -66,23 +66,6 @@ public class EventService {
         return result;
     }
 
-    public List<EventEntity> findByCurrentDate() {
-        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Calendar calendar = Calendar.getInstance();
-        Date startDate = calendar.getTime();
-        startDate.setHours(0);
-        startDate.setMinutes(0);
-        startDate.setSeconds(0);
-        Date endDate = calendar.getTime();
-        endDate.setHours(23);
-        endDate.setMinutes(59);
-        endDate.setSeconds(59);
-        eventDao.openCurrentSession();
-        List<EventEntity> result = eventDao.findBetweenDates(startDate, endDate);
-        eventDao.closeCurrentSession();
-        return result;
-    }
-
     public List<EventEntity> findByBrowser(String browser) {
         eventDao.openCurrentSession();
         List<EventEntity> result = eventDao.findByBrowser(browser);
